abd21a5402e8388eac9f01f5f7a9d4c4790576a2
mitchlinDEV <35628881+mitchlinDEV@users.noreply.github.com>
2025-11-16 23:33:18 +0100

    Update 20251116000100_add_kind_to_media_files.rb

--- db/migrate/20251116000100_add_kind_to_media_files.rb
+++ db/migrate/20251116000100_add_kind_to_media_files.rb
@@ -2,6 +2,6 @@
 
 class AddKindToMediaFiles < ActiveRecord::Migration[7.0]
   def change
-    add_column :media_indexer_media_files, :kind, :string
+    add_column :discourse_media_indexer_media_files, :kind, :string
   end
 end
