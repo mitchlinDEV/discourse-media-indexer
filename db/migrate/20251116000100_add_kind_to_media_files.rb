3e9db43af5e94349df567dbe08b440bcb6cb3cbf
mitchlinDEV <35628881+mitchlinDEV@users.noreply.github.com>
2025-11-17 10:51:30 +0100

    Update 20251116000100_add_kind_to_media_files.rb

--- db/migrate/20251116000100_add_kind_to_media_files.rb
+++ db/migrate/20251116000100_add_kind_to_media_files.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class AddKindToMediaFiles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :discourse_media_indexer_media_files, :kind, :string
+  def up
+    return unless table_exists?(:media_indexer_files)
+    return if column_exists?(:media_indexer_files, :kind)
+
+    add_column :media_indexer_files, :kind, :string
+  end
+
+  def down
+    return unless table_exists?(:media_indexer_files)
+    return unless column_exists?(:media_indexer_files, :kind)
+
+    remove_column :media_indexer_files, :kind
   end
 end
