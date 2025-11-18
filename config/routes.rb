3602cb06e12f03c65675eff3dd16d4e7509e94bd
mitchlinDEV <35628881+mitchlinDEV@users.noreply.github.com>
2025-11-18 20:01:02 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 Discourse::Application.routes.append do
   namespace :discourse_media_indexer, defaults: { format: :json } do
-    get "/list" => "media#index"   # /discourse_media_indexer/list
+    # Your existing filesystem endpoint:
+    get "/list" => "media#index"    # /discourse_media_indexer/list
+
+    # New DB-backed endpoints (correct controller reference):
+    get "/media-db" => "db_media#index"
+    get "/media-db/:id" => "db_media#show"
   end
 end
